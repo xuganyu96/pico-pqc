c3fef9f67c30679ca2e52da78c9d0ac87b1c1f72
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-04-03 15:56:34 -0400

    Ripped out liboqs

---
+++ src/busybox.c
@@ -0,0 +1,24 @@
+/**
+ * Benchmarking speed for KEMs
+ */
+#include "cyccnt.h"
+#include <inttypes.h>
+#include <pico/stdio.h>
+#include <pico/time.h>
+#include <stdio.h>
+
+#define SLEEP_MS 10
+
+int main(void) {
+  stdio_init_all();
+  enable_dwt();
+
+  while (1) {
+    reset_cyccnt();
+    uint32_t start_time = read_cyccnt();
+    sleep_ms(SLEEP_MS);
+    uint32_t end_time = read_cyccnt();
+    printf("sleep: %3d, cycles: %" PRIu32 "\n", SLEEP_MS,
+           end_time - start_time);
+  }
+}
